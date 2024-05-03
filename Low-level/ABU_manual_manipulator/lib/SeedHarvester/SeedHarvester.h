5e2b0c56a0d9e7604f84c88a012b29e6a82b7634
chaoolongrukter <kaitniyomlove@gmail.com>
2024-05-03 14:39:22 +0700

    adding multi stage control

--- Low-level/ABU_manual_manipulator/lib/SeedHarvester/SeedHarvester.h
+++ Low-level/ABU_manual_manipulator/lib/SeedHarvester/SeedHarvester.h
@@ -3,54 +3,63 @@
 
 #include <Servo.h>
 
-class SeedHarvester
-{
+class SeedHarvester {
 public:
   // gpin refer to *Servo at grabbing position
   // lft A and B refer to lifter motor pin
   SeedHarvester(int gpin, int lftA, int lftB, int Minlimit, int Maxlimit, int stepPin, int dirPin, int setzeropin);
 
-  void setup();  // set pin and gripper position
+  void setup(); // set pin and gripper position
   // stepper
-  void linearDrive(double dis, int dir);  // drive gripper position to disire position and direction
-  void setZero();                         // set the gripper position to the start
+  void linearDrive(double dis, int dir); // drive gripper position to disire position and direction
+  void setZero(); // set the gripper position to the start
   // servo
-  void grab();     // grab an object
-  void release();  // release an object
+  void grab(); // grab an object
+  void release(); // release an object 
   // command function
-  void singleHarvest_locking();  // harvest the object
-  void singleRelease();          // release the harvested obj
+  void singleHarvest_locking(); // harvest the object
+  void singleRelease(); // release the harvested obj
 
-  void stock();
+  void stack();
   void preparing();
+  void pull_up();
+  void pull_down();
+  void Stacking(bool next);
+
+  void locking();
+  void drop();
+  void drop_down();
+  void poping();
+
+  void add_dis(bool log, int dis);
+ 
   // additional from old gripper
-  void lifter_up(int pwm);    // pull the gripper up
-  void lifter_down(int pwm);  // pull the gripper down
+  void lifter_up(int pwm); // pull the gripper up
+  void lifter_down(int pwm); // pull the gripper down
 
-  void lifter_up(int pwm, int mms);    // pull the gripper up with time
-  void lifter_down(int pwm, int mms);  // pull the gripper down with time
+  void lifter_up(int pwm, int mms); // pull the gripper up with time
+  void lifter_down(int pwm, int mms); // pull the gripper down with time
 
-  void single_press();
-
+  void single_press(bool next);
 private:
-  int gap = 80;  // distance between seed in mm
+  int gap = 80;  //distance between seed in mm
 
-  int gpin_;         // grabber pin (servo)
-  int lftA_;         // lifter pin (ref A)
-  int lftB_;         // lifter pin (ref B)
+  int gpin_;      // grabber pin (servo)
+  int lftA_;     // lifter pin (ref A)
+  int lftB_;     // lifter pin (ref B)
   int Minlimitpin_;  // ground limit pin
   int Maxlimitpin_;  // top limit pin
-  int dirPin_;       // Direction pin for the step motor
-  int stepPin_;      // Step pin for the step motor
-  int setzeropin_;   // limit pin to set zero for step motor
+  int dirPin_;    // Direction pin for the step motor
+  int stepPin_;   // Step pin for the step motor
+  int setzeropin_; // limit pin to set zero for step motor
 
   int storage = 0;
-  int manual_lock_dis = 430;  // the initial pos in mm
+  int manual_lock_dis = 430; // the initial pos in mm
 
   int n_release = 0;
 
-  bool Ldir = true;   // go out from set zero pos
-  bool Rdir = false;  // go in to set zero pos
+  bool Ldir = true; // go out from set zero pos
+  bool Rdir = false; // go in to set zero pos
 
   int grbAng = 0;
   int relAng = 90;
@@ -58,6 +67,11 @@ private:
   int pwm = 170;
 
   bool harvest = true;
+  bool ready_to_stack = true;
+  bool pulling = false;
+
+  int stage = 0;
+  int pop_stage = 0;
 };
 
 #endif  // SEEDHARVESTER_H
