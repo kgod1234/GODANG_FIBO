d458596e8f09a0260c4634bbe0b29b2d2fd65a03
Jirattanun Leeudomwong <49854272+Tien-Jirattanun@users.noreply.github.com>
2024-05-05 12:19:28 +0700

    Fixing micro ros and start with multi core programming

--- Low-level/ABU_auto_mobile/src/main.cpp
+++ Low-level/ABU_auto_mobile/src/main.cpp
@@ -1,38 +1,46 @@
+// RP2040 dual-core example
+// Gary Sims October 2022
+
+// RP2040: Arduino code will normally execute only on core 0,
+// with the 2nd core sitting idle in a low power state.
+
 #include <Arduino.h>
-#include <FreeRTOS.h>
-#include <task.h>
 
-// Task function for Core 0
-void taskCore0(void* parameter)
-{
-  for (;;)
-  {
-    Serial.println("Task running on Core 0");
-    vTaskDelay(500 / portTICK_PERIOD_MS);
-  }
-}
 
-// Task function for Core 1
-void taskCore1(void* parameter)
-{
-  for (;;)
-  {
-    Serial.println("Task running on Core 1");
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-  }
-}
+int count0, count1;
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  while (!Serial)
+  {
+    delay(1);  // wait for serial port to connect.
+  }
 
-  // Create tasks on each core
-  xTaskCreate(taskCore0, "TaskCore0", 128, NULL, 1, NULL);  // Core 0
-  xTaskCreate(taskCore1, "TaskCore1", 128, NULL, 1, NULL);  // Core 1
+  count0 = 0;
+}
+
+void setup1()
+{
+  count1 = 1000;
 }
 
 void loop()
 {
-  // Code in loop() will not be executed in this configuration
-  // because tasks are managed by FreeRTOS
+  Serial.print("loop0():");
+  Serial.println(count0);
+  count0++;
+  if (count0 > 1000)
+    count0 = 0;
+  delay(1000);
+}
+
+void loop1()
+{
+  Serial.print("loop1():");
+  Serial.println(count1);
+  count1--;
+  if (count1 <= 0)
+    count1 = 1000;
+  delay(2500);
 }
